ddbf1e958cdb89f8194d0d93558f019f80eb1381
Alden <NjmeDine@hotmail.com>
2017-12-07 22:09:29 +0800

    Create Type Utilities

---
+++ src/Extendable/Utils/TypeUtil.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+
+namespace Extendable.Utils
+{
+    public class TypeUtil
+    {
+        public static T ChangeType<T>(object value, T defaultValue = default(T))
+        {
+            try
+            {
+                var t = typeof(T);
+
+                if (t.IsEnum)
+                {
+                    return (T)Enum.Parse(t.UnderlyingSystemType, value.ToString());
+                }
+
+                if (t.IsGenericType && t.GetGenericTypeDefinition() == typeof(Nullable<>))
+                {
+                    if (value == null)
+                    {
+                        return default(T);
+                    }
+
+                    t = Nullable.GetUnderlyingType(t);
+                }
+
+                return (T)Convert.ChangeType(value, t, CultureInfo.InvariantCulture);
+            }
+            catch
+            {
+                return defaultValue;
+            }
+        }
+    }
+}
